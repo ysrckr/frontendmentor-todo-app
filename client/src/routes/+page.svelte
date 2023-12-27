827433ac2a0b56008b58d67b19d984ac5d3fc913
Yasar Cakir <yasar.cakir@mirsis.com.tr>
2023-12-27 23:39:47 +0300

    Add dark mode toggle functionality and update styles

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,11 +1,44 @@
 <script lang="ts">
+import cn from 'classnames'
+type Mode = 'light' | 'dark'
+let mode: Mode = 'light'
 
+const toggleMode = () => {
+  mode = mode === 'light' ? 'dark' : 'light'
+}
 
 
 </script>
 
+
 <main class="main">
-  <h1>todo</h1>
+<div class={cn('background', {
+  'background--dark': mode === 'dark'
+}
+)}>
+<div class="header">
+  <h1 class="header__logo">todo</h1>
+  <button on:click={toggleMode} class="header__toggle">
+{#if mode === 'light'}
+  <img src="/images/icon-moon.svg" alt="moon">
+{:else}
+  <img src="/images/icon-sun.svg" alt="sun">
+{/if}
+  </button>
+</div>
+<form class="form">
+  <input class="form__input" type="text" name="" id="">
+</form>
+<ul class="list">
+  <li class="list__item">
+    <div class="todo">
+      <input class="todo__checkbox" type="checkbox" name="" id="">
+      <label class="todo__label" for="">Complete online JavaScript course</label>
+      <img class="todo__delete" src="/images/icon-cross.svg" alt="delete">
+    </div>
+  </li>
+</ul>
+</div>
 </main>
 
 <style lang="scss" global>
