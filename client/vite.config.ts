f2ca8ebab6523fa378b0f9e2c424450e36a39375
Yasar Cakir <yasar.cakir@mirsis.com.tr>
2023-12-28 20:53:37 +0300

    Add server configuration to Vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+
+	server: {
+		port: 3000,
+		hmr: {
+			port: 443
+		},
+    open: true,
+    
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
