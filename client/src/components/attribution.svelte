9b95b4465fd601cd2efbc07b4e636bd1d389e409
Yasar Cakir <yasar.cakir@mirsis.com.tr>
2023-12-28 10:45:09 +0300

    Add class "attribution" to main component

---
+++ client/src/components/attribution.svelte
@@ -0,0 +1,4 @@
+  <div class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel=”noopener >Frontend Mentor</a>. 
+    Coded by <a href="portfolio.yasarcakir.com" target="_blank" rel=”noopener >Yasar Cakir</a>.
+  </div>
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -2,6 +2,7 @@
 import cn from 'classnames'
 import type { Todo } from '$lib/types/todo'
 import TodoItem  from '../components/todo.svelte';
+	import Attribution from '../components/attribution.svelte';
 
 type Mode = 'light' | 'dark'
 let mode: Mode = 'light'
@@ -47,30 +48,31 @@ export let todos: Todo[] = [
 
 
 <main class="main">
-<div class={cn('background', {
-  'background--dark': mode === 'dark'
-}
-)}>
-<div class="header">
-  <h1 class="header__logo">todo</h1>
-  <button on:click={toggleMode} class="header__toggle">
-{#if mode === 'light'}
-  <img src="/images/icon-moon.svg" alt="moon">
-{:else}
-  <img src="/images/icon-sun.svg" alt="sun">
-{/if}
-  </button>
-</div>
-<form class="form">
-  <label for="form--todo"></label>
-  <input class="form__input" type="text" name="todo" id="form--todo">
-</form>
-<ul class="list">
-{#each todos as todo (todo.id)}
-  <TodoItem {todo}/>
-{/each}
-</ul>
-</div>
+  <div class={cn('background', {
+    'background--dark': mode === 'dark'
+  }
+  )}>
+    <div class="header">
+      <h1 class="header__logo">todo</h1>
+      <button on:click={toggleMode} class="header__toggle">
+    {#if mode === 'light'}
+      <img src="/images/icon-moon.svg" alt="moon">
+    {:else}
+      <img src="/images/icon-sun.svg" alt="sun">
+    {/if}
+      </button>
+    </div>
+    <form class="form">
+      <label for="form--todo"></label>
+      <input class="form__input" type="text" name="todo" id="form--todo">
+    </form>
+    <ul class="list">
+    {#each todos as todo (todo.id)}
+      <TodoItem {todo}/>
+    {/each}
+    </ul>
+  </div>
+  <Attribution />
 </main>
 
 <style lang="scss" global>
