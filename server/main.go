6815a99eaffc1a54a28114d419a8c200543e65d6
Yasar Cakir <yasar.cakir@mirsis.com.tr>
2024-01-01 20:32:27 +0300

    Add server configuration and basic HTTP server implementation

--- server/main.go
+++ server/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
 func main() {
-	
+
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello!"))
+	})
+
+	http.ListenAndServe(":8000", r)
 }
