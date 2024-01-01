07ef42ccaa6a3ee51e9a7525fa700c5638400f0a
Yasar Cakir <yasar.cakir@mirsis.com.tr>
2024-01-01 21:31:15 +0300

    Add API routes for todos

--- server/main.go
+++ server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -12,21 +13,31 @@ func main() {
 
 	r := chi.NewRouter()
 
+	apiRouter := chi.NewRouter()
+
+	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(cors.Handler(cors.Options{
 		AllowedOrigins: []string{"http://localhost:3000"},
 		// AllowedOrigins: []string{"https://*", "http://*"},
 		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedMethods:   []string{"GET", "POST", "PATCH", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: false,
-		MaxAge:           300, // Maximum value not ignored by any of major browsers
+		MaxAge:           300,
 	}))
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello!"))
+	apiRouter.Route("/todos", func(r chi.Router) {
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+
+			fmt.Fprintf(w, "hello todos")
+
+		})
+
 	})
+
+	r.Mount("/api/v1", apiRouter)
 
 	http.ListenAndServe(":8000", r)
 }
