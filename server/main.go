d0e983267e861896812cd25533e853fb6b55b25c
Yasar Cakir <yasar.cakir@mirsis.com.tr>
2024-01-01 20:49:28 +0300

    Update allowed origins to only allow requests from "http://localhost:3000".

--- server/main.go
+++ server/main.go
@@ -14,8 +14,8 @@ func main() {
 
 	r.Use(middleware.Logger)
 	r.Use(cors.Handler(cors.Options{
-		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
-		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedOrigins: []string{"http://localhost:3000"},
+		// AllowedOrigins: []string{"https://*", "http://*"},
 		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
