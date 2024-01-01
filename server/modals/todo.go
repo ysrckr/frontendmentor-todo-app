7092a2252ff693adca3b6772a8cdc3298dc1521c
Yasar Cakir <yasar.cakir@mirsis.com.tr>
2024-01-01 21:35:16 +0300

    Refactor schema and modal structures

--- server/modals/todo.go
+++ server/modals/todo.go
@@ -4,10 +4,8 @@ import (
 	_ "github.com/lib/pq"
 )
 
-
 type Todo struct {
-  Id int            `db:"id" json:"id"`
-  Text string       `db:"text" json:"text"`
-  Completed bool    `db:"completed" json:"completed"`
-  UserId int        `db:"user_id" json:"user_id"`
+	Id        int    `db:"id" json:"id"`
+	Text      string `db:"text" json:"text"`
+	Completed bool   `db:"completed" json:"completed"`
 }
--- server/modals/user.go
+++
@@ -1,6 +0,0 @@
-package modals
-
-type User struct {
-  Id int      `db:"id" json:"id"`
-  Ip string    `db:"ip" json:"ip"`
-}
