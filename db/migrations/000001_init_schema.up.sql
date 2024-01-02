555408a2d686a5a5237dacb4b1a590f7afa392b4
Yasar Cakir <yasar.cakir@mirsis.com.tr>
2024-01-02 23:03:05 +0300

    Update schema: add serial primary key and default values

--- db/migrations/000001_init_schema.up.sql
+++ db/migrations/000001_init_schema.up.sql
@@ -1,7 +1,8 @@
+-- Active: 1704121805152@@127.0.0.1@5432@todoapp
 CREATE TABLE "todos" (
-  "id" integer PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "text" varchar,
-  "completed" boolean,
+  "completed" boolean DEFAULT (false),
   "created_at" timestamp DEFAULT (now()),
   "updated_at" timestamp DEFAULT (now())
 );
