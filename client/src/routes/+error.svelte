b2fde7e482061c5c5a55896252beb9378c759b06
Yasar Cakir <yasar.cakir@mirsis.com.tr>
2024-01-05 22:27:56 +0300

    Add error page component

---
+++ client/src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div class="error">
+	<h1>{$page.status}: {$page?.error?.message}</h1>
+</div>
+
+<style lang="scss">
+	.error {
+		margin: 25% auto;
+		text-align: center;
+	}
+</style>
